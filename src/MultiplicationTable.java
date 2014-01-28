4f46ca288b0ebd60c9167b96a848783761a3d71b
nikhithareddy02 <nikhithareddy02@gmail.com>
2014-01-28 17:29:59 +0530

    commit

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,42 @@
+
+
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class MultiplicationTable extends HttpServlet {
+ 
+  private String message;
+
+  public void init() throws ServletException
+  {
+     
+	// Do required initialization
+	  
+      message ="Multiplication table" ;
+  }
+
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+            throws ServletException, IOException
+  {
+      // Set response content type
+      response.setContentType("text/html");
+
+      // Actual logic goes here.
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + message + "</h1>");
+      int j=0;
+      for (int i = 1; i <= 10; i++) {
+    	  j=10*i;
+    	  out.println("<br>10 * "+i+"="+j);
+	}
+  }
+  
+  public void destroy()
+  {
+      // do nothing.
+  }
+}
